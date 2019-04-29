040bd99d7714ac1617b7cefcd29ff7e701da5418
MichalAW <michal.wanta@gmail.com>
2019-04-29 23:58:17 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,57 +1,42 @@
-
-
-// var element =
-// React.createElement('div', {},
-//     React.createElement('h1', {}, 'Lista filmów'),
-//     React.createElement('ul', {},
-//       	React.createElement('li', {},
-// 	        React.createElement('h2', {}, 'Harry Potter'),
-// 	        React.createElement('p', {}, 'Film o czarodzieju')
-//       	),
-//     	React.createElement('li', {},
-// 	        React.createElement('h2', {}, 'Król Lew'),
-// 	        React.createElement('p', {}, 'Film opowiadający historię króla sawanny')
-//       	)
-//     )
-// );
-
 var movies = [
   	{
-	    id: 1,
-	    title: 'Harry Potter',
-	    desc: 'film o czarodzieju',
+        id: 1,
+        title: 'Harry Potter',
+        desc: 'film o czarodzieju',
         src: './images/potter.jpg'
   	},
   	{
-	    id: 2,
-	    title: 'Król Lew',
-	    desc: 'Film o królu sawanny',
+        id: 2,
+        title: 'Król Lew',
+        desc: 'Film o królu sawanny',
         src: './images/lion.jpg'
   	},
-  	  	{
-	    id: 3,
-	    title: 'Władca Pierścieni',
-	    desc: 'film o władcy pierścieni',
+  	{
+        id: 3,
+        title: 'Władca Pierścieni',
+        desc: 'film o władcy pierścieni',
         src: './images/lord.jpg'
   	},
   	{
-	    id: 4,
-	    title: 'Gra o tron',
-	    desc: 'Film o królewskich rodach',
+        id: 4,
+        title: 'Gra o tron',
+        desc: 'Film o królewskich rodach',
         src: './images/game.jpg'
   	}
 ];
 
 var moviesElements = movies.map(function(movie) {
-  	return React.createElement('li', {key: movie.id},
-      	React.createElement('h2', {}, movie.title),
-      	React.createElement('p', {}, movie.desc),
-      	React.createElement('img', {src: movie.src, width: "250px", height: "300px"}),
+    return React.createElement('li', {key: movie.id},
+        React.createElement('h2', {}, movie.title),
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img', {src: movie.src, alt: "movie-image"}),
     );
 });
+
 var element =
 React.createElement('div', {},
-	React.createElement('h1', {}, 'Lista filmów'),
-	React.createElement('ul', {}, moviesElements),
+    React.createElement('h1', {}, 'Lista filmów'),
+    React.createElement('ul', {}, moviesElements),
 );
+
 ReactDOM.render(element, document.getElementById('app'));
