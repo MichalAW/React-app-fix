3f91206deee05358003df3ec646e9057a35a6973
MichalAW <michal.wanta@gmail.com>
2019-04-29 21:58:16 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-import React from 'react';
-import potter from '../images/potter.jpg';
-import lion from '../images/lion.jpg';
-import lord from '../images/lord.jpg';
-import game from '../images/game.jpg';
+
 
 // var element =
 // React.createElement('div', {},
@@ -24,25 +20,25 @@ var movies = [
 	    id: 1,
 	    title: 'Harry Potter',
 	    desc: 'film o czarodzieju',
-        image: {potter}
+        <img src="potter.jpg" alt="potter">
   	},
   	{
 	    id: 2,
 	    title: 'Król Lew',
 	    desc: 'Film o królu sawanny',
-        image: {lion}
+        <img src="lion.jpg" alt="lion">
   	},
   	  	{
 	    id: 3,
 	    title: 'Władca Pierścieni',
 	    desc: 'film o władcy pierścieni',
-        image: {lord}
+        <img src="lord.jpg" alt="lord">
   	},
   	{
 	    id: 4,
 	    title: 'Gra o tron',
 	    desc: 'Film o królewskich rodach',
-        image: {game}
+        <img src="game.jpg" alt="game">
   	}
 ];
 
@@ -50,7 +46,7 @@ var moviesElements = movies.map(function(movie) {
   	return React.createElement('li', {key: movie.id},
       	React.createElement('h2', {}, movie.title),
       	React.createElement('p', {}, movie.desc),
-      	React.createElement('img', {}, movie.image),
+      	React.createElement('img', {src: movie.src, width: "250px", height: "300px"}),
     );
 });
 var element =
