1d06b75b1bacace76abae967f5948e26fc817e3e
MichalAW <michal.wanta@gmail.com>
2019-04-29 15:54:23 +0200

    add img, bug report

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import potter from '../images/potter.jpg';
+import lion from '../images/lion.jpg';
+import lord from '../images/lord.jpg';
+import game from '../images/game.jpg';
+
 // var element =
 // React.createElement('div', {},
 //     React.createElement('h1', {}, 'Lista filmów'),
@@ -18,25 +24,25 @@ var movies = [
 	    id: 1,
 	    title: 'Harry Potter',
 	    desc: 'film o czarodzieju',
-	    img: 'src="/images/potter.jpg"'
+        image: {potter}
   	},
   	{
 	    id: 2,
 	    title: 'Król Lew',
 	    desc: 'Film o królu sawanny',
-	    img: 'src="/images/lion.jpg"'
+        image: {lion}
   	},
   	  	{
 	    id: 3,
 	    title: 'Władca Pierścieni',
 	    desc: 'film o władcy pierścieni',
-	    img: 'src="/images/lord.jpg"'
+        image: {lord}
   	},
   	{
 	    id: 4,
 	    title: 'Gra o tron',
 	    desc: 'Film o królewskich rodach',
-	    img: 'src="/images/game.jpg"'
+        image: {game}
   	}
 ];
 
@@ -44,12 +50,12 @@ var moviesElements = movies.map(function(movie) {
   	return React.createElement('li', {key: movie.id},
       	React.createElement('h2', {}, movie.title),
       	React.createElement('p', {}, movie.desc),
-      	React.createElement('img', {}, movie.img),
+      	React.createElement('img', {}, movie.image),
     );
 });
 var element =
 React.createElement('div', {},
 	React.createElement('h1', {}, 'Lista filmów'),
-	React.createElement('ul', {}, moviesElements)
+	React.createElement('ul', {}, moviesElements),
 );
 ReactDOM.render(element, document.getElementById('app'));
